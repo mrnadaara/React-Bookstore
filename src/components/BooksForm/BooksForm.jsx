19b1fd50840c001529d7a7e8749b99d37559c240
Leo <ronald.mauritsson@gmail.com>
2020-06-25 18:48:34 +0300

    Implemented BookForm component

--- src/components/BooksForm/BooksForm.jsx
+++ src/components/BooksForm/BooksForm.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 class BooksForm extends React.Component {
   render() {
+    const categories = ["Action", "Biography", "History", "Horror", "Kids", "Learning", "Sci-Fi"]
     return (
-      <div>Books Form</div>
+      <div>
+        <form>
+          <input name="book-title"  value="book-title"/>
+          <select name="category">
+            { categories.map(category => (
+              <option key={category} >{ category }</option>
+            )) }
+          </select>
+          <button type="submit">Save</button>
+        </form>
+      </div>
     );
+  }
 }
 
 export default BooksForm
